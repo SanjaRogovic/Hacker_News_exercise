7397079292fce15964d71f89e7fea4fd1fb2c3d5
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-08-12 19:34:12 +0200

    update

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import * as ReactBootstrap from "react-bootstrap";
-import Stack from 'react-bootstrap/Stack';
+
 
 
 const Data = () => {
