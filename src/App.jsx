8ac99852dbfd3409a7da3ee38dd1aa965cdb893d
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-08-12 15:38:48 +0200

    update jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
 import './App.css'
-import {useState, useEffect} from "react"
 import Data from "./Components/Data"
 
 
 function App() {
-
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-
-  const fetchData = () => {
-    fetch("http://hn.algolia.com/api/v1/items/:id")
-    .then ((response) => {
-      if(!response.ok) {
-        throw newError (`The fetch has failed with status ${response.status}`)
-      }
-
-      return response.json()
-    })
-
-    .then((data) => {
-      setData(data)
-    })
-
-    .catch((error) => {
-      console.log(error, "error")
-    })
-
-    .finally(() => {
-      console.log("fetch completed")
-    })
-
-  }
-  
-  
 
   return (
     <>
--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,36 +1,80 @@
-import {useState} from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Data = () => {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-    const [inputValue, setInputValue] = useState("")
+  
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-    const handleSubmit = (e) => {
-        e.prevent.default()
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.prevent.default();
+
+    try{
+        const displayData = await axios(`http://hn.algolia.com/api/v1/search?query=${inputValue}`);
+        if(!displayData) throw new Error(`The request has failed with status ${displayData.status}`);
+        const response = await displayData.data.hits;
+        setData ([response]);
     }
-
-    const handleChange = (e) => {
-        setInputValue(e.target.value)
+    
+    catch(error) {
+        console.log(error, "ERROR");
     }
+  };
 
 
-    return (
-        <div>
-            <h1>Hacker News</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" value={inputValue} onChange={handleChange}/>
-                    <button type="submit">Submit</button>
-                </div>             
-                <div>
-                    <ul>
-                        <li>
+//   const fetchData = () => {
+//     fetch(`http://hn.algolia.com/api/v1/search?query=${inputValue}`)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error(`The request has failed with status ${response.status}`);
+//         }
 
-                        </li>
-                    </ul>
-                </div>              
-            </form>
+//         return response.json();
+//       }) 
+
+//       .then((data) => {
+//         setData(data.hits);
+//         // console.log(data.hits)
+//       })
+
+//       .catch((error) => {
+//         console.log(error, "ERROR");
+//       })
+
+//       .finally(() => {
+//         console.log("FETCH COMPLETED");
+//       });
+//   };
+
+  return (
+    <div>
+      <h2>Hacker News</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={inputValue}
+          onChange={handleChange}
+        />   
+        <button type="submit">Search</button>
+      </form>
+      {data.length ? (
+        <div key={response.created_at}>
+          <h3>{response.title}</h3>
+          <p>{response.url}</p>
         </div>
-    )
-}
+      ): <p>Error loading data</p>}
+    </div>
+  );
+};
 
 export default Data;
